17a8e4e23cf2fbc395ddf4a46ed228de50731603
ayatamine <ayatir04@gmail.com>
2021-11-20 16:54:22 +0100

    using bootstrap cdns

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,10 +25,12 @@ export default {
       link: [
         this.$i18n.locale == 'en' ? {
              rel: "stylesheet",
-             href: `/css/bootstrap.css`
+             href: `https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css`
         } :
         {
-            rel:"stylesheet" ,href:"/css/bootstrap-rtl.css" ,
+            rel:"stylesheet" ,href:"https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css" ,
+            integrity:"sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe",
+            crossorigin:"anonymous"
         },
         ...i18nHead.link
       ]
